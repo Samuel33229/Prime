96fd9303d92c3d391715439f35a8724fb345468e
Samuel33229 <samuelsontsf@gmail.com>
2023-12-03 22:14:14 -0400

    Push

---
+++ JS/test-1.js
@@ -0,0 +1,26 @@
+// let num = 0
+// let num2 = 5
+
+// console.log(num + num2) 
+// console.log(num * num2)
+// console.log(num - num2)
+// console.log(num / num2)
+
+let boton = document.getElementById("btn-c")
+
+boton.addEventListener("click", () => {
+    window.alert('Hola, comprame unas luces porfa')
+})
+
+// Igual igual ==
+// <= Menor o igual que
+// >= Mayor o igual que
+// > Mayor
+// < Menor
+// != Diferente que 
+
+// 2==2&&3==4
+
+// 2==2||3==4
+
+// Algebra Booleana//
